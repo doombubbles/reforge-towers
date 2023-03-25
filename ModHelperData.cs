eceec953f9f41f5dd56ecc70146101f4ce79fb93
doombubbles <obsidiantoolzg@gmail.com>
2023-03-25 08:58:27 -0700

    Initial Release

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -10,7 +10,7 @@ public static class ModHelperData
         "Lets you reforge towers in a similar way to how you can reforge items in Terraria.\n" +
         "The price of reforging a tower is proportional to its total worth. However, reforges will also affect the costs of a tower's available upgrades, with positive reforges making them more expensive, and negative reforges making them cheaper.\n" +
         "\n" +
-        "Note: for Heroes, the price multiplier is applied to XP instead.";
+        "Note: for Heroes, the price multiplier is applied to XP gains instead.";
 
     public const string RepoOwner = "doombubbles";
     public const string RepoName = "reforge-towers";
